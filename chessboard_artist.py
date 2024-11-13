533f5f0557a56ca908de76da8a5576692123ffda
johndoknjas <jdoknjas@sfu.ca>
2024-11-13 09:02:02 -0800

    A python script that draws a chessboard according to colour specifications for squares, and then writes to a png file.

---
+++ chessboard_artist.py
@@ -0,0 +1,20 @@
+from PIL import Image, ImageDraw
+import time
+import sys
+
+def make_board_png(board_size: int, light_square: str, dark_square: str) -> Image:
+    square_size = board_size // 8
+    image = Image.new('RGB', (board_size, board_size), light_square)
+    draw = ImageDraw.Draw(image)
+    for row in range(8):
+        for col in range(8):
+            if (row + col) % 2 != 0:
+                x0, y0 = col * square_size, row * square_size
+                x1, y1 = x0 + square_size, y0 + square_size
+                draw.rectangle([x0, y0, x1, y1], fill=dark_square)
+    return image
+
+print(sys.argv)
+light, dark = sys.argv[1:]
+board_image = make_board_png(2048, light, dark) # for a 2048x2048 pixel board
+board_image.save(f"{time.time()}.png")
