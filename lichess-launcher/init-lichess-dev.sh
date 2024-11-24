bd0dca86e3196233478e1acbdd4da82b9818eb79
johndoknjas <jdoknjas@sfu.ca>
2024-11-24 02:01:49 -0800

    Tell the user what the commands to run are.

--- lichess-launcher/init-lichess-dev.sh
+++ lichess-launcher/init-lichess-dev.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 LILA_DIR="/home/johndoknjas/lichess/take-3/lila"
 LILA_WS_DIR="/home/johndoknjas/lichess/take-3/lila-ws"
-tmux new-session -d -s my_session "cd '$LILA_DIR'; bash"
+tmux new-session -d -s my_session "cd '$LILA_DIR'; echo 'Command: redis-server'; bash"
 tmux setw remain-on-exit on
-tmux split-window -h "cd '$LILA_DIR'; bash"
-tmux split-window -v "cd '$LILA_WS_DIR'; bash"
+tmux split-window -h "cd '$LILA_DIR'; echo 'Command: mongod'; bash"
+tmux split-window -v "cd '$LILA_WS_DIR'; echo 'Command: sbt run -Dcsrf.origin=http://localhost:9663'; bash"
 tmux select-pane -t 0
-tmux split-window -v "cd '$LILA_DIR'; bash"
+tmux split-window -v "cd '$LILA_DIR'; echo 'Command: ui/build -w'; bash"
 tmux -2 attach-session -d
