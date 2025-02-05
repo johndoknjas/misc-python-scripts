d4cdfae6ebc081d92d3e7d2272b4007d224efd1c
johndoknjas <jdoknjas@sfu.ca>
2025-02-05 03:19:35 -0800

    Kill any currently running redis db before doing the `redis-server` command.

--- lichess-launcher/init-lichess-dev.sh
+++ lichess-launcher/init-lichess-dev.sh
@@ -4,7 +4,7 @@ LILA_WS_DIR="/home/johndoknjas/lichess/take-3/lila-ws"
 tmux new-session -d -s my_session "cd '$LILA_DIR' && ./lila.sh; bash"
 tmux setw remain-on-exit on
 tmux split-window -h "cd '$LILA_DIR' && ui/build -w; bash"
-tmux split-window -v "cd '$LILA_DIR' && redis-server; bash"
+tmux split-window -v "cd '$LILA_DIR' && killall redis-server && sleep 1; redis-server; bash"
 tmux select-pane -t 0
 tmux split-window -v "cd '$LILA_DIR' && mongod; bash"
 tmux split-window -h "cd '$LILA_WS_DIR' && sbt run -Dcsrf.origin=http://localhost:9663; bash"
