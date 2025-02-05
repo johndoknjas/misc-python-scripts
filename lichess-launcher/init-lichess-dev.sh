5ab0d216fa4f022fe64c80150a56a809d0becfaa
johndoknjas <jdoknjas@sfu.ca>
2025-02-05 02:56:29 -0800

    Update script to automatically run each of the commands in each window, rather than just telling the user them.

--- lichess-launcher/init-lichess-dev.sh
+++ lichess-launcher/init-lichess-dev.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 LILA_DIR="/home/johndoknjas/lichess/take-3/lila"
 LILA_WS_DIR="/home/johndoknjas/lichess/take-3/lila-ws"
-tmux new-session -d -s my_session "cd '$LILA_DIR'; echo 'Command: ./lila.sh'; bash"
+tmux new-session -d -s my_session "cd '$LILA_DIR' && ./lila.sh; bash"
 tmux setw remain-on-exit on
-tmux split-window -h "cd '$LILA_DIR'; echo 'Command: ui/build -w'; bash"
-tmux split-window -v "cd '$LILA_DIR'; echo 'Command: redis-server'; bash"
+tmux split-window -h "cd '$LILA_DIR' && ui/build -w; bash"
+tmux split-window -v "cd '$LILA_DIR' && redis-server; bash"
 tmux select-pane -t 0
-tmux split-window -v "cd '$LILA_DIR'; echo 'Command: mongod'; bash"
-tmux split-window -h "cd '$LILA_WS_DIR'; echo 'Command: sbt run -Dcsrf.origin=http://localhost:9663'; bash"
+tmux split-window -v "cd '$LILA_DIR' && mongod; bash"
+tmux split-window -h "cd '$LILA_WS_DIR' && sbt run -Dcsrf.origin=http://localhost:9663; bash"
 tmux swap-pane -s 1 -t 2
 tmux -2 attach-session -d
